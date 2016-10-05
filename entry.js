9ff200237d36544be8981d374d52918c91294709
Giordan Battaglin <gio@giordan.ca>
2016-10-04 20:33:36 -0400

    simplified functions

--- entry.js
+++ entry.js
@@ -7,26 +7,85 @@ var $ = require('jquery');
 var Counter = React.createClass({
   getInitialState: function() {
     return {
-      counter: null
+      counter: null,
+			error: false,
+			loading: false
     }
   },
 
   render: function() {
     console.log(this.state);
-    return <div>
-      <div>Counter: { this.state.counter }</div>
-      <div><button onClick={ this.increment }>Add One</button></div>
-    </div>
+		if (this.state.loading){
+			return <div>Loading...</div>
+		} else{
+			return <div>
+				{ this.error() }
+				{ this.state.error ? <div>Error, did not compute</div> : null }
+				<div>Counter: { this.state.counter }</div>
+				<div><button onClick={ this.increment }>Add One</button></div>
+				<div><button onClick={ this.decrement }>Minus One</button></div>
+				<div><button onClick={ this.reset }>Reset</button></div>
+			</div>
+		}
   },
+	
+	error: function(){
+		if (this.state.error){
+			return <div>Error, did not compute</div>
+		}
+	},
 
   increment: function() {
+		
     var newCount = this.state.counter + 1;
-
+		var oldCount = this.state.counter;
+		
+		this.updateDatabase(oldCount, newCount);
+		
+    this.setState({counter: newCount});
+  },
+	
+	decrement: function() {
+		
+    var newCount = this.state.counter - 1;
+		var oldCount = this.state.counter;
+		
+		this.updateDatabase(oldCount, newCount);
+		
+    this.setState({counter: newCount});
+  },
+	
+	reset: function() {
+    var newCount = this.state.counter = 0;
+    var oldCount = this.state.counter;
+		
+		this.updateDatabase(oldCount, newCount);
+		
+    this.setState({counter: newCount});
+  },
+	
+	updateDatabase: function(oldCount, newCount) {
+		var component = this;
+		$.ajax({
+			url: "https://dat-counter.firebaseio.com/.json",
+			method: "PUT",
+			data: JSON.stringify(newCount),
+			error: function(){
+				component.setState({counter: oldCount, error: true});
+			}
+		})		
     this.setState({counter: newCount});
   },
 
   componentDidMount: function() {
-    var component = this;
+		var component = this;
+    $.ajax({
+			url: "https://dat-counter.firebaseio.com/.json",
+			method: "GET",
+			success: function(data){
+				component.setState({counter: data, loading: false})
+			}
+		})
   }
 });
 
